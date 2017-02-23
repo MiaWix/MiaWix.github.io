a02a271818d58aebf3d7a1e0b1761ae9e5f35b7c
Mia Wicks <mcw21@scasd.org>
2017-02-23 21:20:07 +0000

    That Was Easy with Space

--- that_was_easy/main.js
+++ that_was_easy/main.js
@@ -1,5 +1,16 @@
-function saythatwasEasy() {
-var thatwasEasy = new Audio("that_was_easy.mp3");
-thatwasEasy.play();
+function sayThatWasEasy() {
+  var thatWasEasy = new Audio("that_was_easy.mp3");
+  thatWasEasy.play();
 }
-$("#easy").on("click", saythatwasEasy);
+
+$("#easy").on("click", sayThatWasEasy);
+
+$(document).keypress(delegateKeypress);
+
+function delegateKeypress(event) {
+    if (event.charCode == 32) {
+        $("#easy").trigger("click");
+    }
+}
+
+
